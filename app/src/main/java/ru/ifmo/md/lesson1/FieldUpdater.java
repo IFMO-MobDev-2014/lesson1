5269198ba96d993c08bc67be8b8e78308feca6f8
Dmitry Tomp <dmitrytomp@gmail.com>
2014-09-12 23:56:03 +0400

    Optimized field drawing; FPS = 16

---
+++ app/src/main/java/ru/ifmo/md/lesson1/FieldUpdater.java
@@ -0,0 +1,75 @@
+package ru.ifmo.md.lesson1;
+
+import android.util.Log;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Created by dimatomp on 12.09.14.
+ */
+public class FieldUpdater implements Runnable {
+    static final String TAG = "FieldUpdater";
+    static final int MAX_FRAMES = 10;
+    final ArrayBlockingQueue<int[][]> queue = new ArrayBlockingQueue<>(MAX_FRAMES);
+    int width, height, field[][];
+
+    public FieldUpdater(int width, int height) {
+        this.width = width;
+        this.height = height;
+        initField();
+    }
+
+    public int[][] nextState() {
+        try {
+            return queue.take();
+        } catch (InterruptedException ignore) {
+            return null;
+        }
+    }
+
+    void initField() {
+        field = new int[width][height];
+        Random rand = new Random();
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++) {
+                field[x][y] = rand.nextInt(FieldDrawer.MAX_COLOR);
+            }
+        }
+        try {
+            queue.put(field);
+        } catch (InterruptedException ignore) {}
+    }
+
+    @Override
+    public void run() {
+        while (WhirlView.running) {
+            int[][] field2 = new int[width][height];
+            for (int x=0; x<width; x++) {
+                for (int y=0; y<height; y++) {
+
+                    field2[x][y] = field[x][y];
+
+                    for (int dx=-1; dx<=1; dx++) {
+                        for (int dy=-1; dy<=1; dy++) {
+                            int x2 = x + dx;
+                            int y2 = y + dy;
+                            if (x2<0) x2 += width;
+                            if (y2<0) y2 += height;
+                            if (x2>=width) x2 -= width;
+                            if (y2>=height) y2 -= height;
+                            if ( (field[x][y]+1) % FieldDrawer.MAX_COLOR == field[x2][y2]) {
+                                field2[x][y] = field[x2][y2];
+                            }
+                        }
+                    }
+                }
+            }
+            try {
+                queue.put(field2);
+            } catch (InterruptedException ignore) {}
+            Log.v(TAG, "Queue size: " + queue.size());
+            field = field2;
+        }
+    }
+}
--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -1,114 +1,92 @@
 package ru.ifmo.md.lesson1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.Log;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
 * Created by thevery on 11/09/14.
 */
-class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
-    final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    SurfaceHolder holder;
-    Thread thread = null;
-    volatile boolean running = false;
+class WhirlView extends SurfaceView {
+    static final String TAG = "WhirlView";
+    public static volatile boolean running = false;
+    Bitmap toDraw;
+    FieldDrawer drawerRunnable;
+    Thread updater, drawer;
+    Timer showTimer;
+    FPSLogger perfLog;
 
     public WhirlView(Context context) {
         super(context);
-        holder = getHolder();
     }
 
     public void resume() {
+        if (getWidth() == 0 || getHeight() == 0)
+            return;
         running = true;
-        thread = new Thread(this);
-        thread.start();
+        FieldUpdater updaterRunnable = new FieldUpdater(getWidth() / FieldDrawer.SCALE, getHeight() / FieldDrawer.SCALE);
+        drawerRunnable = new FieldDrawer(updaterRunnable, getWidth(), getHeight());
+        updater = new Thread(updaterRunnable);
+        drawer = new Thread(drawerRunnable);
+        updater.start();
+        drawer.start();
+        showTimer = new Timer();
+        showTimer.schedule(new DisplayTask(), 0, 16);
+        perfLog = new FPSLogger();
+        showTimer.schedule(perfLog, 0, 1000);
     }
 
     public void pause() {
         running = false;
         try {
-            thread.join();
+            updater.join();
+            drawer.join();
+            showTimer.cancel();
         } catch (InterruptedException ignore) {}
     }
 
-    public void run() {
-        while (running) {
-            if (holder.getSurface().isValid()) {
-                long startTime = System.nanoTime();
-                Canvas canvas = holder.lockCanvas();
-                updateField();
-                onDraw(canvas);
-                holder.unlockCanvasAndPost(canvas);
-                long finishTime = System.nanoTime();
-                Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException ignore) {}
-            }
-        }
-    }
-
     @Override
-    public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
-        initField();
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        resume();
     }
-
-    void initField() {
-        field = new int[width][height];
-        Random rand = new Random();
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                field[x][y] = rand.nextInt(MAX_COLOR);
-            }
-        }
-    }
-
-    void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-
-                field2[x][y] = field[x][y];
-
-                for (int dx=-1; dx<=1; dx++) {
-                    for (int dy=-1; dy<=1; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
-                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
-                        }
-                    }
-                }
-            }
-        }
-        field = field2;
-    }
-
-    Paint paint = new Paint();
 
     @Override
     public void onDraw(Canvas canvas) {
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+        super.onDraw(canvas);
+        if (toDraw != null)
+            canvas.drawBitmap(toDraw, 0, 0, null);
+    }
+
+    class DisplayTask extends TimerTask {
+        @Override
+        public void run() {
+            if (getHolder().getSurface().isValid()) {
+                perfLog.tick();
+                toDraw = drawerRunnable.nextBitmap();
+                Canvas canvas = getHolder().lockCanvas();
+                onDraw(canvas);
+                getHolder().unlockCanvasAndPost(canvas);
             }
+        }
+    }
+
+    class FPSLogger extends TimerTask {
+        int frameCount;
+
+        public synchronized void tick() {
+            frameCount++;
+        }
+
+        @Override
+        public synchronized void run() {
+            Log.d(TAG, "FPS: " + frameCount);
+            frameCount = 0;
         }
     }
 }
