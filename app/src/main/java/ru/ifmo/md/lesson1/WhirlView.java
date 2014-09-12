7f7143bb53cd15a5c15efb92cf48557f0de28870
Berdnikov Pavel <berpaval@gmail.com>
2014-09-13 01:11:25 +0400

    Reorganizing draw principle

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -1,8 +1,9 @@
 package ru.ifmo.md.lesson1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -14,9 +15,10 @@ import java.util.Random;
 */
 class WhirlView extends SurfaceView implements Runnable {
     int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
+    int deviceWidth = 0;
+    int deviceHeight = 0;
+    int width = 240;
+    int height = 320;
     final int MAX_COLOR = 10;
     int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     SurfaceHolder holder;
@@ -51,17 +53,19 @@ class WhirlView extends SurfaceView implements Runnable {
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
                 Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException ignore) {}
             }
         }
     }
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
+        deviceWidth = w;
+        deviceHeight = h;
+        if (w < h && width > height || w > h && width < height) {
+            int t = width;
+            width = height;
+            height = t;
+        }
         initField();
     }
 
@@ -102,12 +106,14 @@ class WhirlView extends SurfaceView implements Runnable {
 
     @Override
     public void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+        int[] colors = new int[width*height];
+        int c = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                colors[c++] = palette[field[x][y]];
             }
         }
+        Bitmap b = Bitmap.createBitmap(colors, width, height, Bitmap.Config.RGB_565);
+        canvas.drawBitmap(b, new Rect(0,0,width-1,height-1), new Rect(0,0,deviceWidth-1,deviceHeight-1), null);
     }
 }
