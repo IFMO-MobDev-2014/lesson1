677682a1b1c6f7ca14605823c4724e187f0f8d4a
mikita95 <niklemark@gmail.com>
2014-09-14 01:55:06 +0400

    improve files

--- app/src/main/java/ru/ifmo/md/lesson1/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson1/MyActivity.java
@@ -2,6 +2,8 @@ package ru.ifmo.md.lesson1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MyActivity extends Activity {
@@ -11,6 +13,8 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         whirlView = new WhirlView(this);
         setContentView(whirlView);
     }
