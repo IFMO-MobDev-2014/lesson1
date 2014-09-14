6d9b42c9c64827418a30acccf8ecc5073bd696c4
Anton Belyy <anton.belyy@gmail.com>
2014-09-14 04:12:06 +0400

    some obvious changes

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -13,12 +13,13 @@ import java.util.Random;
 * Created by thevery on 11/09/14.
 */
 class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
+    int [][] field = null, field2 = null;
     int width = 0;
     int height = 0;
     int scale = 4;
     final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    final int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    final Paint[] colors = new Paint[palette.length];
     SurfaceHolder holder;
     Thread thread = null;
     volatile boolean running = false;
@@ -26,6 +27,12 @@ class WhirlView extends SurfaceView implements Runnable {
     public WhirlView(Context context) {
         super(context);
         holder = getHolder();
+
+        for (int i=0; i<palette.length; i++) {
+            Paint paint = new Paint();
+            paint.setColor(palette[i]);
+            colors[i] = paint;
+        }
     }
 
     public void resume() {
@@ -51,9 +58,6 @@ class WhirlView extends SurfaceView implements Runnable {
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
                 Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException ignore) {}
             }
         }
     }
@@ -67,6 +71,7 @@ class WhirlView extends SurfaceView implements Runnable {
 
     void initField() {
         field = new int[width][height];
+        field2 = new int[width][height];
         Random rand = new Random();
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
@@ -76,37 +81,72 @@ class WhirlView extends SurfaceView implements Runnable {
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-
-                field2[x][y] = field[x][y];
-
-                for (int dx=-1; dx<=1; dx++) {
-                    for (int dy=-1; dy<=1; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
-                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
-                        }
-                    }
+        for (int x=1; x<width-1; x++) {
+            for (int y=1; y<height-1; y++) {
+                int curColor = field[x][y] + 1;
+                int newColor;
+                if (curColor >= MAX_COLOR) {
+                    curColor -= MAX_COLOR;
                 }
+                /*if (x2<0) x2 += width;
+                if (y2<0) y2 += height;
+                if (x2>=width) x2 -= width;
+                if (y2>=height) y2 -= height;*/
+                newColor = field[x + 1][y + 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x + 1][y];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x + 1][y - 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x][y + 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x][y - 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x - 1][y + 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x - 1][y];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                newColor = field[x - 1][y - 1];
+                if (curColor == newColor) {
+                    field2[x][y] = newColor;
+                    continue;
+                }
+                field2[x][y] = field[x][y];
             }
         }
-        field = field2;
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++) {
+                field[x][y] = field2[x][y];
+            }
+        }
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
-                Paint paint = new Paint();
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, colors[field[x][y]]);
             }
         }
     }
