66fcf4a687af44131424933f8b691241c4400d53
Kuznetsov Nikolay <kna.rus@gmail.com>
2014-09-14 19:35:47 +0400

    Some cleanup and refactoring

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -13,7 +13,7 @@ import java.util.concurrent.CyclicBarrier;
 /**
 * Created by thevery on 11/09/14.
 */
-class WhirlView extends SurfaceView implements Runnable {
+class WhirlView extends SurfaceView {
 
     // since most modern phones have at least double-core cpu, why not add another thread to draw
     // and update field in parallel?
@@ -30,33 +30,56 @@ class WhirlView extends SurfaceView implements Runnable {
         }
     }
 
-    volatile CyclicBarrier barrier = null;
-    int [][] field1 = null;
-    int [][] field2 = null;
-    int [] pixels = null;
-    long lastFrameStart = 0;
-    int width = 0;
-    int height = 0;
-    float scaleX = 0.0f;
-    float scaleY = 0.0f;
-    final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    SurfaceHolder holder;
-    Thread thread = null;
-    UpdaterThread updaterThread = null;
-    volatile boolean running = false;
+    private class DrawThread extends Thread {
+        @Override
+        public void run() {
+            while (running) {
+                if (holder.getSurface().isValid()) {
+                    try {
+                        barrier.await();
+                        long startTime = System.nanoTime();
+                        Log.i("TIME","Since last frame: " + (startTime - lastFrameStart) / 1000000);
+                        lastFrameStart = startTime;
+                        Canvas canvas = holder.lockCanvas();
+                        doDraw(canvas);
+                        holder.unlockCanvasAndPost(canvas);
+                        barrier.await();
+                    } catch (InterruptedException ignore) {} catch (BrokenBarrierException ignore) {}
+                    swapFields();
+                }
+            }
+        }
+    }
+
+    private volatile CyclicBarrier barrier = null;
+    private int [][] fieldFront = null;
+    private int [][] fieldBack = null;
+    private int [] pixels = null;
+    private long lastFrameStart = 0;
+    private int width = 0;
+    private int height = 0;
+    private float scaleX = 0.0f;
+    private float scaleY = 0.0f;
+    private final int SIZE_LONG = 320;
+    private final int SIZE_SHORT = 240;
+    private int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    private final int MAX_COLOR = palette.length; // makes sense to do it this way
+    private SurfaceHolder holder;
+    private DrawThread drawThread = null;
+    private UpdaterThread updaterThread = null;
+    private volatile boolean running = false;
 
     public WhirlView(Context context) {
         super(context);
         holder = getHolder();
-        pixels = new int[320*240];
+        pixels = new int[SIZE_LONG * SIZE_SHORT];
     }
 
     public void resume() {
         running = true;
         barrier = new CyclicBarrier(2);
-        thread = new Thread(this);
-        thread.start();
+        drawThread = new DrawThread();
+        drawThread.start();
         updaterThread = new UpdaterThread();
         updaterThread.start();
     }
@@ -67,63 +90,48 @@ class WhirlView extends SurfaceView implements Runnable {
         barrier = new CyclicBarrier(1); // make other threads pass barrier.await transparently
         cb.reset(); // break threads that are already awaiting
         try {
-            thread.join();
+            drawThread.join();
             updaterThread.join();
         } catch (InterruptedException ignore) {}
         barrier = null;
     }
 
-    public void run() {
-        while (running) {
-            if (holder.getSurface().isValid()) {
-                try {
-                    barrier.await();
-                    long startTime = System.nanoTime();
-                    Log.i("TIME","Since last frame: " + (startTime - lastFrameStart) / 1000000);
-                    lastFrameStart = startTime;
-                    Canvas canvas = holder.lockCanvas();
-                    doDraw(canvas);
-                    holder.unlockCanvasAndPost(canvas);
-                    barrier.await();
-                } catch (InterruptedException ignore) {} catch (BrokenBarrierException ignore) {}
-                swapFields();
-            }
-        }
-    }
-
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        pause();
+        boolean wasRunning = running;
+        if(running)
+            pause();
         if (w > h) {
-            width = 320;
-            height = 240;
+            width = SIZE_LONG;
+            height = SIZE_SHORT;
             scaleX = w / (float) width;
             scaleY = h / (float) height;
         } else {
-            width = 240;
-            height = 320;
+            width = SIZE_SHORT;
+            height = SIZE_LONG;
             scaleX = w / (float) width;
             scaleY = h / (float) height;
         }
         initField();
-        resume();
+        if(wasRunning)
+            resume();
     }
 
-    void initField() {
-        field1 = new int[width][height];
-        field2 = new int[width][height];
+    private void initField() {
+        fieldFront = new int[width][height];
+        fieldBack = new int[width][height];
         Random rand = new Random();
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
-                field1[x][y] = rand.nextInt(MAX_COLOR);
+                fieldFront[x][y] = rand.nextInt(MAX_COLOR);
             }
         }
     }
 
-    void updateSingleField(int x, int y) {
-        field2[x][y] = field1[x][y];
+    private void updateSingleField(int x, int y) {
+        fieldBack[x][y] = fieldFront[x][y];
 
-        int nc = field1[x][y] + 1;
+        int nc = fieldFront[x][y] + 1;
         if (nc == MAX_COLOR) { // faster than %, even in java
             nc = 0;
         }
@@ -138,23 +146,23 @@ class WhirlView extends SurfaceView implements Runnable {
                 if (x2>=width) x2 -= width;
                 if (y2>=height) y2 -= height;
 
-                if (nc == field1[x2][y2]) {
-                    field2[x][y] = field1[x2][y2];
+                if (nc == fieldFront[x2][y2]) {
+                    fieldBack[x][y] = fieldFront[x2][y2];
                     break outer;
                 }
             }
         }
     }
 
-    void updateField() {
+    private void updateField() {
         int wm1 = width - 1;
         int hm1 = height - 1;
 
         for (int x=1; x < wm1; x++)
             for (int y=1; y < hm1; y++) {
-                field2[x][y] = field1[x][y];
+                fieldBack[x][y] = fieldFront[x][y];
 
-                int nc = field1[x][y] + 1;
+                int nc = fieldFront[x][y] + 1;
                 if (nc == MAX_COLOR) {
                     nc = 0;
                 }
@@ -165,8 +173,8 @@ class WhirlView extends SurfaceView implements Runnable {
                         int x2 = x + dx;
                         int y2 = y + dy;
 
-                        if (nc == field1[x2][y2]) {
-                            field2[x][y] = field1[x2][y2];
+                        if (nc == fieldFront[x2][y2]) {
+                            fieldBack[x][y] = fieldFront[x2][y2];
                             break outer;
                         }
                     }
@@ -184,17 +192,17 @@ class WhirlView extends SurfaceView implements Runnable {
         }
     }
 
-    void swapFields() {
-        int [][] tf = field1;
-        field1 = field2;
-        field2 = tf;
+    private void swapFields() {
+        int[][] tf = fieldFront;
+        fieldFront = fieldBack;
+        fieldBack = tf;
     }
 
-    public void doDraw(Canvas canvas) {
+    private void doDraw(Canvas canvas) {
         canvas.scale(scaleX, scaleY);
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                pixels[x+y*width] = palette[field1[x][y]];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                pixels[x + y * width] = palette[fieldFront[x][y]];
             }
         }
         canvas.drawBitmap(pixels, 0, width, 0, 0, width, height, false, null);
