73f003ef6e6c36a4456326d9ae0278e8af8381d3
Dmitry Nekrasov <d04101995@mail.ru>
2014-09-15 22:41:19 +0400

    bitmaps introduced. seems to be final...

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -2,10 +2,11 @@ package ru.ifmo.md.lesson1;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 
 import java.util.Random;
 
@@ -16,13 +17,15 @@ class WhirlView extends SurfaceView implements Runnable {
     int [][] field = null;
     int [][] field2 = null;
     int [][] work = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
-    Paint paint = new Paint();
+    int [] pixels = null;
+    final int width = 240;
+    final int height = 320;
+    int thisPixel;
     final int MAX_COLOR = 10;
     int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     SurfaceHolder holder;
+    Bitmap bitmap;
+    Rect rect;
     Thread thread = null;
     volatile boolean running = false;
 
@@ -60,14 +63,16 @@ class WhirlView extends SurfaceView implements Runnable {
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
+        rect = new Rect(0, 0, w, h);
+
         initField();
     }
 
     void initField() {
         field = new int[width][height];
         field2 = new int[width][height];
+        pixels = new int[width * height];
+        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Random rand = new Random();
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
@@ -98,15 +103,18 @@ class WhirlView extends SurfaceView implements Runnable {
         work = field;
         field = field2;
         field2 = work;
+
+        thisPixel = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixels[thisPixel++] = palette[field[x][y]];
+            }
+        }
     }
 
     @Override
     public void draw(Canvas canvas) {
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
-            }
-        }
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        canvas.drawBitmap(bitmap, null, rect, null);
     }
 }
