744d1b0e2d779fbaca81bfb8453489f5b95764cf
Антон Краснокутский <antonk@MacBook-Pro-Anton.local>
2014-09-15 21:34:17 +0400

    optimized

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -1,8 +1,10 @@
 package ru.ifmo.md.lesson1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -13,19 +15,25 @@ import java.util.Random;
 * Created by thevery on 11/09/14.
 */
 class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
-    final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    SurfaceHolder holder;
-    Thread thread = null;
-    volatile boolean running = false;
+    byte [][][] field           = null;
+    byte currentBuffer          = 0;
+    int scale                   = 0;
+    final int MAX_COLOR         = 10;
+    final int WIDTH             = 240;
+    final int HEIGHT            = 320;
+    Bitmap bitmap               = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+    int[] colors                = new int[WIDTH * HEIGHT];
+    int[] palette               = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00,
+            0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    SurfaceHolder holder        = null;
+    Thread thread               = null;
+    volatile boolean running    = false;
+    Rect rect                   = null;
 
     public WhirlView(Context context) {
         super(context);
         holder = getHolder();
+        field = new byte[2][HEIGHT + 2][WIDTH + 2];
     }
 
     public void resume() {
@@ -51,63 +59,74 @@ class WhirlView extends SurfaceView implements Runnable {
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
                 Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException ignore) {}
             }
         }
     }
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
         initField();
+        rect = new Rect(0, 0, w, h);
+    }
+
+    void updateBounds(int bufferNumber) {
+        for (int x = 1; x < HEIGHT + 1; x++) {
+            field[bufferNumber][x][0] = field[bufferNumber][x][WIDTH];
+            field[bufferNumber][x][WIDTH + 1] = field[bufferNumber][x][1];
+        }
+        for (int y = 1; y < WIDTH + 1; y++) {
+            field[bufferNumber][0][y] = field[bufferNumber][HEIGHT][y];
+            field[bufferNumber][HEIGHT + 1][y] = field[bufferNumber][1][y];
+        }
+        field[bufferNumber][0][0] = field[bufferNumber][HEIGHT][WIDTH];
+        field[bufferNumber][HEIGHT + 1][WIDTH + 1] = field[bufferNumber][1][1];
+        field[bufferNumber][HEIGHT + 1][0] = field[bufferNumber][1][WIDTH];
+        field[bufferNumber][0][WIDTH + 1] = field[bufferNumber][HEIGHT][1];
     }
 
     void initField() {
-        field = new int[width][height];
+        currentBuffer = 1;
         Random rand = new Random();
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                field[x][y] = rand.nextInt(MAX_COLOR);
+        for (int x = 1; x < HEIGHT + 1; x++) {
+            for (int y = 1; y < WIDTH + 1; y++) {
+                field[0][x][y] = (byte)rand.nextInt(MAX_COLOR);
             }
         }
+        updateBounds(0);
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
+        for (int x = 1; x < HEIGHT + 1; x++) {
+            for (int y = 1; y < WIDTH + 1; y++) {
 
-                field2[x][y] = field[x][y];
+                field[currentBuffer][x][y] = field[currentBuffer ^ 1][x][y];
 
-                for (int dx=-1; dx<=1; dx++) {
-                    for (int dy=-1; dy<=1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
                         int x2 = x + dx;
                         int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
-                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
+                        if ((field[currentBuffer ^ 1][x][y] + 1) % MAX_COLOR ==
+                                field[currentBuffer ^ 1][x2][y2]) {
+                            field[currentBuffer][x][y] = field[currentBuffer ^ 1][x2][y2];
                         }
                     }
                 }
             }
         }
-        field = field2;
+        updateBounds(currentBuffer);
+        currentBuffer ^= 1;
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                Paint paint = new Paint();
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+        int currentIndex = 0;
+        for (int y = 1; y < HEIGHT + 1; y++) {
+            for (int x = 1; x < WIDTH + 1; x++) {
+                colors[currentIndex++] = palette[field[currentBuffer ^ 1][y][x]];
             }
         }
+        bitmap.setPixels(colors, 0, WIDTH, 0, 0, WIDTH, HEIGHT);
+        canvas.drawBitmap(bitmap, null, rect, null);
     }
 }
+
