6448f497f97c83512a53b380e993ad5972b5fbd5
itegulov <wibkwibk@gmail.com>
2014-09-15 20:04:31 +0400

    using bitmap instead of drawing rectangles, added threads, some small optimizations

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -2,111 +2,213 @@ package ru.ifmo.md.lesson1;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.Random;
 
-/**
-* Created by thevery on 11/09/14.
-*/
-class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
+class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
+    int MAX_COLOR = 10;
+    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080,
+            0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    char[][] field, bufferField, temp;
+    int[] colors;
     int width = 0;
     int height = 0;
-    int scale = 4;
-    final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    SurfaceHolder holder;
-    Thread thread = null;
-    volatile boolean running = false;
+    float scaleX, scaleY;
+    DrawThread drawThread;
 
     public WhirlView(Context context) {
         super(context);
-        holder = getHolder();
+        getHolder().addCallback(this);
     }
 
     public void resume() {
-        running = true;
-        thread = new Thread(this);
-        thread.start();
+        if (drawThread != null) {
+            drawThread.setRunning(true);
+        }
     }
 
     public void pause() {
-        running = false;
-        try {
-            thread.join();
-        } catch (InterruptedException ignore) {}
-    }
-
-    public void run() {
-        while (running) {
-            if (holder.getSurface().isValid()) {
-                long startTime = System.nanoTime();
-                Canvas canvas = holder.lockCanvas();
-                updateField();
-                onDraw(canvas);
-                holder.unlockCanvasAndPost(canvas);
-                long finishTime = System.nanoTime();
-                Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
-                try {
-                    Thread.sleep(16);
-                } catch (InterruptedException ignore) {}
-            }
+        if (drawThread != null) {
+            drawThread.setRunning(false);
         }
     }
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
+        width = 240;
+        height = 320;
+        scaleX = (float)(w) / width;
+        scaleY = (float)(h) / height;
         initField();
     }
 
     void initField() {
-        field = new int[width][height];
+        colors = new int[height * width];
+        field = new char[width][height];
+        bufferField = new char[width][height];
         Random rand = new Random();
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                field[x][y] = rand.nextInt(MAX_COLOR);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                field[x][y] = (char)rand.nextInt(MAX_COLOR);
             }
+        }
+    }
+
+    void updateColors(int beginX, int beginY, int endX, int endY) {
+        int x2, y2;
+        for (int y = beginY; y < endY; y++) {
+            for (int x = beginX; x < endX; x++) {
+                bufferField[x][y] = field[x][y];
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        x2 = x + dx;
+                        y2 = y + dy;
+                        if (x2 < 0) {
+                            x2 += width;
+                        }
+                        if (y2 < 0) {
+                            y2 += height;
+                        }
+                        if (x2 >= width) {
+                            x2 -= width;
+                        }
+                        if (y2 >= height) {
+                            y2 -= height;
+                        }
+                        if (field[x][y] + 1 == MAX_COLOR) {
+                            if (0 == field[x2][y2]) {
+                                bufferField[x][y] = field[x2][y2];
+                            }
+                        } else {
+                            if (field[x][y] + 1 == field[x2][y2]) {
+                                bufferField[x][y] = field[x2][y2];
+                            }
+                        }
+                    }
+                }
+                colors[y * width + x] = palette[field[x][y]];
+            }
+        }
+    }
+
+    class Updater extends Thread {
+        private int beginX, beginY, endX, endY;
+
+        Updater(int beginX, int beginY, int endX, int endY) {
+            this.beginX = beginX;
+            this.beginY = beginY;
+            this.endX = endX;
+            this.endY = endY;
+        }
+
+        @Override
+        public void run() {
+            updateColors(beginX, beginY, endX, endY);
         }
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
+        Thread leftTop = new Updater(0, 0, width / 2, height / 2);
 
-                field2[x][y] = field[x][y];
+        Thread rightTop = new Updater(width / 2, 0, width, height / 2);
 
-                for (int dx=-1; dx<=1; dx++) {
-                    for (int dy=-1; dy<=1; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
-                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
-                        }
-                    }
-                }
-            }
+        Thread leftBottom = new Updater(0, height / 2, width / 2, height);
+
+        Thread rightBottom = new Updater(width / 2, height / 2, width, height);
+
+        leftTop.start();
+        rightTop.start();
+        leftBottom.start();
+        rightBottom.start();
+        try {
+            leftTop.join();
+            rightTop.join();
+            leftBottom.join();
+            rightBottom.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        field = field2;
+
+        temp = field;
+        field = bufferField;
+        bufferField = temp;
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                Paint paint = new Paint();
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        drawThread = new DrawThread(getHolder());
+        drawThread.setRunning(true);
+        drawThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        drawThread.setRunning(false);
+        boolean trying = true;
+        while (trying) {
+            try {
+                drawThread.join();
+                trying = false;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class DrawThread extends Thread {
+        FPSCounter fpsCounter = new FPSCounter();
+        private boolean running = false;
+        private SurfaceHolder holder;
+
+        public DrawThread(SurfaceHolder holder) {
+            this.holder = holder;
+        }
+
+        public void setRunning(boolean run) {
+            running = run;
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                long startTime = System.nanoTime();
+                WhirlView.this.updateField();
+                long finishTime = System.nanoTime();
+                Log.i("TIME", "Update field: " + (finishTime - startTime) / 1000000);
+                startTime = finishTime;
+                Canvas canvas = holder.lockCanvas(null);
+                try {
+                    canvas.scale(scaleX, scaleY);
+                    canvas.drawBitmap(colors, 0, width, 0, 0, width, height, false, null);
+                } catch (NullPointerException e) {
+                    continue;
+                }
+                holder.unlockCanvasAndPost(canvas);
+                finishTime = System.nanoTime();
+                Log.i("TIME", "Drawing: " + (finishTime - startTime) / 1000000);
+                fpsCounter.logFrame();
+            }
+        }
+
+        public class FPSCounter {
+            long startTime = System.nanoTime();
+            int frames = 0;
+
+            public void logFrame() {
+                frames++;
+                if (System.nanoTime() - startTime >= 1000000000) {
+                    Log.d("FPSCounter", "fps: " + frames);
+                    frames = 0;
+                    startTime = System.nanoTime();
+                }
             }
         }
     }
