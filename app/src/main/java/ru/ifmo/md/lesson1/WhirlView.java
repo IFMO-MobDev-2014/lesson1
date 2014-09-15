d9665e9ddd73f6b240d0ace765b2a9d596d0b142
Maxim Slyusarenko <slyuslyu.m@mail.ru>
2014-09-15 20:30:44 +0400

    Final version

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -9,19 +9,21 @@ import android.view.SurfaceView;
 
 import java.util.Random;
 
-/**
-* Created by thevery on 11/09/14.
-*/
 class WhirlView extends SurfaceView implements Runnable {
     int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
+    int [][] field2 = null;
+    int [] colors = null;
+    final int width = 240;
+    final int height = 320;
+    float scalew = 0;
+    float scaleh = 0;
     final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    final int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     SurfaceHolder holder;
     Thread thread = null;
     volatile boolean running = false;
+    Random rand;
+    Canvas canvas;
 
     public WhirlView(Context context) {
         super(context);
@@ -45,9 +47,9 @@ class WhirlView extends SurfaceView implements Runnable {
         while (running) {
             if (holder.getSurface().isValid()) {
                 long startTime = System.nanoTime();
-                Canvas canvas = holder.lockCanvas();
+                canvas = holder.lockCanvas();
                 updateField();
-                onDraw(canvas);
+                draw(canvas);
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
                 Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
@@ -60,14 +62,15 @@ class WhirlView extends SurfaceView implements Runnable {
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
+        scalew = (float) w/width;
+        scaleh = (float) h/height;
         initField();
     }
 
     void initField() {
         field = new int[width][height];
-        Random rand = new Random();
+        colors = new int[width * height];
+        rand = new Random();
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
                 field[x][y] = rand.nextInt(MAX_COLOR);
@@ -76,23 +79,176 @@ class WhirlView extends SurfaceView implements Runnable {
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
+        field2 = new int[width][height];
+        field2[0][0] = field[0][0];
+        if ((field[0][0] + 1) % MAX_COLOR == field[width - 1][height - 1]) {
+            field2[0][0] = field[width - 1][height - 1];
+        }
+
+        for (int dy=0; dy<=1; dy++) {
+            if ((field[0][0] + 1) % MAX_COLOR == field[width - 1][dy]) {
+                field2[0][0] = field[width - 1][dy];
+            }
+        }
+
+        for (int dx=0; dx<=1; dx++) {
+            if ((field[0][0] + 1) % MAX_COLOR == field[dx][height - 1]) {
+                field2[0][0] = field[dx][height - 1];
+            }
+            for (int dy=0; dy<=1; dy++) {
+                if ((field[0][0] + 1) % MAX_COLOR == field[dx][dy]) {
+                    field2[0][0] = field[dx][dy];
+                }
+            }
+        }
+
+        //выше обработали точку (0, 0). Теперь надо обработать остальные краевые точки
+
+        for (int y=1; y<height - 1; y++) {
+            field2[0][y] = field[0][y];
+            for (int dy=-1; dy<=1; dy++) {
+                if ((field[0][y] + 1) % MAX_COLOR == field[width - 1][y + dy]) {
+                    field2[0][y] = field[width - 1][y + dy];
+                }
+            }
+            for (int dx=0; dx<=1; dx++) {
+                for (int dy=-1; dy<=1; dy++) {
+                    if ((field[0][y] + 1) % MAX_COLOR == field[dx][y + dy]) {
+                        field2[0][y] = field[dx][y + dy];
+                    }
+                }
+            }
+        }
+        //первая строка
+
+        for (int y=1; y<height - 1; y++) {
+            field2[width - 1][y] = field[width - 1][y];
+            for (int dx = -1; dx <= 0; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    if ((field[width - 1][y] + 1) % MAX_COLOR == field[width - 1 + dx][y + dy]) {
+                        field2[width - 1][y] = field[width - 1 + dx][y + dy];
+                    }
+                }
+            }
+            for (int dy=-1; dy<=1; dy++) {
+                if ((field[width - 1][y] + 1) % MAX_COLOR == field[0][y + dy]) {
+                    field2[width - 1][y] = field[0][y + dy];
+                }
+            }
+        }
+        //последняя строка
+
+        for (int x=1; x<width - 1; x++) {
+            field2[x][0] = field[x][0];
+            for (int dx=-1; dx<=1; dx++) {
+                if ((field[x][0] + 1) % MAX_COLOR == field[x + dx][height - 1]) {
+                    field2[x][0] = field[x + dx][height - 1];
+                }
+                for (int dy=0; dy<=1; dy++) {
+                    if ((field[x][0] + 1) % MAX_COLOR == field[x + dx][dy]) {
+                        field2[x][0] = field[x + dx][dy];
+                    }
+                }
+            }
+        }
+        //первый столбец
+
+        for (int x=1; x<width - 1; x++) {
+            field2[x][height - 1] = field[x][height - 1];
+            for (int dx=-1; dx<=1; dx++) {
+                for (int dy=-1; dy<=0; dy++) {
+                    if ((field[x][height - 1] + 1) % MAX_COLOR == field[x + dx][height - 1 + dy]) {
+                        field2[x][height - 1] = field[x + dx][height - 1 + dy];
+                    }
+                }
+                if ((field[x][height - 1] + 1) % MAX_COLOR == field[x][0]) {
+                    field2[x][height - 1] = field[x][0];
+                }
+            }
+        }
+        //последний столбец
+
+        field2[width - 1][height - 1] = field[width - 1][height - 1];
+        for (int dx=-1; dx<=0; dx++) {
+            for (int dy=-1; dy<=0; dy++) {
+                if ((field[width - 1][height - 1] + 1) % MAX_COLOR == field[width - 1 + dx][height - 1 + dy]) {
+                    field2[width - 1][height - 1] = field[width - 1 + dx][height - 1 + dy];
+                }
+            }
+            if ((field[width - 1][height - 1] + 1) % MAX_COLOR == field[width - 1 + dx][0]) {
+                field2[width - 1][height - 1] = field[width - 1 + dx][0];
+            }
+        }
+        for (int dy=-1; dy<=0; dy++) {
+            if ((field[width - 1][height - 1] + 1) % MAX_COLOR == field[0][height - 1 + dy]) {
+                field2[width - 1][height - 1] = field[0][height - 1 + dy];
+            }
+        }
+        if ((field[width - 1][height - 1] + 1) % MAX_COLOR == field[0][0]) {
+            field2[width - 1][height - 1] = field[0][0];
+        }
+        //правый нижний угол
+
+        field2[0][height - 1] = field[0][height - 1];
+        for (int dy=-1; dy<=0; dy++) {
+            if ((field[0][height - 1] + 1) % MAX_COLOR == field[width - 1][height - 1 + dy]) {
+                field2[0][height - 1] = field[width - 1][height - 1 + dy];
+            }
+        }
+        if ((field[0][height - 1] + 1) % MAX_COLOR == field[width - 1][0]) {
+            field2[0][height - 1] = field[width - 1][0];
+        }
+        for (int dx=0; dx<=1; dx++) {
+            for (int dy=-1; dy<=0; dy++) {
+                if ((field[0][height - 1] + 1) % MAX_COLOR == field[dx][height - 1 + dy]) {
+                    field2[0][height - 1] = field[dx][height - 1 + dy];
+                }
+            }
+            if ((field[0][height - 1] + 1) % MAX_COLOR == field[dx][0]) {
+                field2[0][height - 1] = field[dx][0];
+            }
+        }
+        //правый верхний угол
+
+        field2[width - 1][0] = field[width - 1][0];
+        for (int dx=-1; dx<=0; dx++) {
+            if ((field[width - 1][0] + 1) % MAX_COLOR == field[width - 1 + dx][height - 1]) {
+                field2[width - 1][0] = field[width - 1 + dx][height - 1];
+            }
+            for (int dy=0; dy<=1; dy++) {
+                if ((field[width - 1][0] + 1) % MAX_COLOR == field[width - 1 + dx][dy]) {
+                    field2[width - 1][0] = field[width - 1 + dx][dy];
+                }
+            }
+        }
+        if ((field[width - 1][0] + 1) % MAX_COLOR == field[0][height - 1]) {
+            field2[width - 1][0] = field[0][height - 1];
+        }
+        for (int dy=0; dy<=1; dy++) {
+            if ((field[width - 1][0] + 1) % MAX_COLOR == field[0][dy]) {
+                field2[width - 1][0] = field[0][dy];
+            }
+        }
+        //левый нижний угол
+
+        for (int x=1; x<width - 1; x++) {
+            for (int y=1; y<height - 1; y++) {
 
                 field2[x][y] = field[x][y];
 
+                boolean flag = false;
                 for (int dx=-1; dx<=1; dx++) {
                     for (int dy=-1; dy<=1; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
+                        int x2 = x - dx;
+                        int y2 = y - dy;
                         if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
                             field2[x][y] = field[x2][y2];
+                            flag = true;
+                            break;
                         }
+                    }
+                    if (flag) {
+                        break;
                     }
                 }
             }
@@ -101,13 +257,13 @@ class WhirlView extends SurfaceView implements Runnable {
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
+    public void draw(Canvas canvas) {
+        canvas.scale(scalew, scaleh);
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
-                Paint paint = new Paint();
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
+                colors[x + y * width] = palette[field[x][y]];
             }
         }
+        canvas.drawBitmap(colors, 0, width, 0, 0, width, height, false, null);
     }
 }
