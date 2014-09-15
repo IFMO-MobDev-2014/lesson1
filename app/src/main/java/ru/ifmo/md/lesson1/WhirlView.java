f03ea1d69a8994252ba91d7404e82fa3cf721a28
itegulov <wibkwibk@gmail.com>
2014-09-15 21:43:51 +0400

    added thread for processing updateField independently from drawing, made code to look neat

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -16,11 +16,14 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
     int[][] bufferField;
     int[][] tempField;
     int[] colors;
-    int width = 0;
-    int height = 0;
+    final int width = 240;
+    final int height = 320;
     float scaleX;
     float scaleY;
+    final Object locker = new Object();
+    boolean updatingField = false;
     DrawThread drawThread;
+    FieldUpdater fieldUpdater;
 
     public WhirlView(Context context) {
         super(context);
@@ -31,18 +34,24 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         if (drawThread != null) {
             drawThread.setRunning(true);
         }
+
+        if (fieldUpdater != null) {
+            fieldUpdater.setRunning(true);
+        }
     }
 
     public void pause() {
         if (drawThread != null) {
             drawThread.setRunning(false);
         }
+
+        if (fieldUpdater != null) {
+            fieldUpdater.setRunning(false);
+        }
     }
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = 240;
-        height = 320;
         scaleX = (float)(w) / width;
         scaleY = (float)(h) / height;
         initField();
@@ -55,7 +64,7 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         Random rand = new Random();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                field[x][y] = (char)rand.nextInt(MAX_COLOR);
+                field[x][y] = rand.nextInt(MAX_COLOR);
             }
         }
     }
@@ -94,114 +103,14 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
-    class Updater extends Thread {
-        private int beginX, beginY, endX, endY;
-
-        Updater(int beginX, int beginY, int endX, int endY) {
-            this.beginX = beginX;
-            this.beginY = beginY;
-            this.endX = endX;
-            this.endY = endY;
-        }
-
-        @Override
-        public void run() {
-            updateColors(beginX, beginY, endX, endY);
-        }
-    }
-
-    void updateField() {
-        Thread top = new Updater(1, 1, width - 1, height / 2);
-        Thread bottom = new Updater(1, height / 2, width - 1, height - 1);
-        top.start();
-        bottom.start();
-        int x2, y2;
-        for (int x = 0; x < width; x++) {
-            bufferField[x][0] = field[x][0];
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    x2 = x + dx;
-                    y2 = dy;
-                    if (x2 < 0) x2 += width;
-                    if (y2 < 0) y2 += height;
-                    if (x2 >= width) x2 -= width;
-                    if (y2 >= height) y2 -= height;
-                    if ( (field[x][0] + 1) % MAX_COLOR == field[x2][y2]) {
-                        bufferField[x][0] = field[x2][y2];
-                    }
-                }
-            }
-        }
-
-        for (int x = 0; x < width; x++) {
-            bufferField[x][height - 1] = field[x][height - 1];
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    x2 = x + dx;
-                    y2 = height - 1 + dy;
-                    if (x2 < 0) x2 += width;
-                    if (y2 < 0) y2 += height;
-                    if (x2 >= width) x2 -= width;
-                    if (y2 >= height) y2 -= height;
-                    if ( (field[x][height - 1] + 1) % MAX_COLOR == field[x2][y2]) {
-                        bufferField[x][height - 1] = field[x2][y2];
-                    }
-                }
-            }
-        }
-
-        for (int y = 0; y < height; y++) {
-            bufferField[0][y] = field[0][y];
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    x2 = dx;
-                    y2 = y + dy;
-                    if (x2 < 0) x2 += width;
-                    if (y2 < 0) y2 += height;
-                    if (x2 >= width) x2 -= width;
-                    if (y2 >= height) y2 -= height;
-                    if ( (field[0][y] + 1) % MAX_COLOR == field[x2][y2]) {
-                        bufferField[0][y] = field[x2][y2];
-                    }
-                }
-            }
-        }
-
-        for (int y = 0; y < height; y++) {
-            bufferField[width - 1][y] = field[width - 1][y];
-
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    x2 = width - 1 + dx;
-                    y2 = y + dy;
-                    if (x2 < 0) x2 += width;
-                    if (y2 < 0) y2 += height;
-                    if (x2 >= width) x2 -= width;
-                    if (y2 >= height) y2 -= height;
-                    if ( (field[width - 1][y] + 1) % MAX_COLOR == field[x2][y2]) {
-                        bufferField[width - 1][y] = field[x2][y2];
-                    }
-                }
-            }
-        }
-
-        try {
-            top.join();
-            bottom.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        tempField = field;
-        field = bufferField;
-        bufferField = tempField;
-    }
-
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         drawThread = new DrawThread(getHolder());
         drawThread.setRunning(true);
         drawThread.start();
+        fieldUpdater = new FieldUpdater();
+        fieldUpdater.setRunning(true);
+        fieldUpdater.start();
     }
 
     @Override
@@ -216,6 +125,7 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         while (trying) {
             try {
                 drawThread.join();
+                fieldUpdater.join();
                 trying = false;
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -223,8 +133,147 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    class FieldUpdater extends Thread {
+        private boolean running = false;
+
+        public void processBoard() {
+            int x2;
+            int y2;
+
+            for (int x = 0; x < width; x++) {
+                bufferField[x][0] = field[x][0];
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        x2 = x + dx;
+                        y2 = dy;
+                        if (x2 < 0) x2 += width;
+                        if (y2 < 0) y2 += height;
+                        if (x2 >= width) x2 -= width;
+                        if (y2 >= height) y2 -= height;
+                        if ( (field[x][0] + 1) % MAX_COLOR == field[x2][y2]) {
+                            bufferField[x][0] = field[x2][y2];
+                        }
+                    }
+                }
+            }
+
+            for (int x = 0; x < width; x++) {
+                bufferField[x][height - 1] = field[x][height - 1];
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        x2 = x + dx;
+                        y2 = height - 1 + dy;
+                        if (x2 < 0) x2 += width;
+                        if (y2 < 0) y2 += height;
+                        if (x2 >= width) x2 -= width;
+                        if (y2 >= height) y2 -= height;
+                        if ( (field[x][height - 1] + 1) % MAX_COLOR == field[x2][y2]) {
+                            bufferField[x][height - 1] = field[x2][y2];
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < height; y++) {
+                bufferField[0][y] = field[0][y];
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        x2 = dx;
+                        y2 = y + dy;
+                        if (x2 < 0) x2 += width;
+                        if (y2 < 0) y2 += height;
+                        if (x2 >= width) x2 -= width;
+                        if (y2 >= height) y2 -= height;
+                        if ( (field[0][y] + 1) % MAX_COLOR == field[x2][y2]) {
+                            bufferField[0][y] = field[x2][y2];
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < height; y++) {
+                bufferField[width - 1][y] = field[width - 1][y];
+
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        x2 = width - 1 + dx;
+                        y2 = y + dy;
+                        if (x2 < 0) x2 += width;
+                        if (y2 < 0) y2 += height;
+                        if (x2 >= width) x2 -= width;
+                        if (y2 >= height) y2 -= height;
+                        if ( (field[width - 1][y] + 1) % MAX_COLOR == field[x2][y2]) {
+                            bufferField[width - 1][y] = field[x2][y2];
+                        }
+                    }
+                }
+            }
+        }
+
+        public void updateField() {
+            Thread top = new Updater(1, 1, width - 1, height / 2);
+            Thread bottom = new Updater(1, height / 2, width - 1, height - 1);
+            top.start();
+            bottom.start();
+
+            processBoard();
+
+            try {
+                top.join();
+                bottom.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (locker) {
+                while (updatingField) {
+                    try {
+                        locker.wait(1);
+                    } catch (InterruptedException ignored) {
+
+                    }
+                }
+                tempField = field;
+                field = bufferField;
+                bufferField = tempField;
+                updatingField = true;
+                locker.notifyAll();
+            }
+        }
+
+        public void setRunning(boolean run) {
+            running = run;
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                updateField();
+            }
+        }
+    }
+
+    class Updater extends Thread {
+        private int beginX;
+        private int beginY;
+        private int endX;
+        private int endY;
+
+        Updater(int beginX, int beginY, int endX, int endY) {
+            this.beginX = beginX;
+            this.beginY = beginY;
+            this.endX = endX;
+            this.endY = endY;
+        }
+
+        @Override
+        public void run() {
+            updateColors(beginX, beginY, endX, endY);
+        }
+    }
+
     class DrawThread extends Thread {
-        FPSCounter fpsCounter = new FPSCounter();
+        private FPSCounter fpsCounter = new FPSCounter();
         private boolean running = false;
         private SurfaceHolder holder;
 
@@ -240,35 +289,44 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
         public void run() {
             while (running) {
                 long startTime = System.nanoTime();
-                WhirlView.this.updateField();
-                long finishTime = System.nanoTime();
-                Log.i("TIME", "Update field: " + (finishTime - startTime) / 1000000);
-                startTime = finishTime;
                 Canvas canvas = holder.lockCanvas(null);
-                try {
-                    canvas.scale(scaleX, scaleY);
-                    canvas.drawBitmap(colors, 0, width, 0, 0, width, height, false, null);
-                } catch (NullPointerException e) {
-                    continue;
+                synchronized (locker) {
+                    while (!updatingField) {
+                        try {
+                            locker.wait(1);
+                        } catch (InterruptedException ignored) {
+
+                        }
+                    }
+                    updatingField = false;
+
+                    try {
+                        canvas.scale(scaleX, scaleY);
+                        canvas.drawBitmap(colors, 0, width, 0, 0, width, height, false, null);
+                    } catch (NullPointerException e) {
+                        locker.notifyAll();
+                        continue;
+                    }
+                    locker.notifyAll();
                 }
                 holder.unlockCanvasAndPost(canvas);
-                finishTime = System.nanoTime();
-                Log.i("TIME", "Drawing: " + (finishTime - startTime) / 1000000);
+                long finishTime = System.nanoTime();
+                Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
                 fpsCounter.logFrame();
             }
         }
+    }
 
-        public class FPSCounter {
-            long startTime = System.nanoTime();
-            int frames = 0;
+    public class FPSCounter {
+        long startTime = System.nanoTime();
+        int frames = 0;
 
-            public void logFrame() {
-                frames++;
-                if (System.nanoTime() - startTime >= 1000000000) {
-                    Log.d("FPSCounter", "fps: " + frames);
-                    frames = 0;
-                    startTime = System.nanoTime();
-                }
+        public void logFrame() {
+            frames++;
+            if (System.nanoTime() - startTime >= 1000000000) {
+                Log.d("FPS", "fps: " + frames);
+                frames = 0;
+                startTime = System.nanoTime();
             }
         }
     }
