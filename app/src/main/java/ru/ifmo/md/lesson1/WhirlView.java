f3a054a3a8889ee645a78a1ab1c65bdef13f3bca
itegulov <wibkwibk@gmail.com>
2014-09-15 21:12:00 +0400

    changed updating threads count to 2, now processing updateField faster

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -12,11 +12,14 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
     int MAX_COLOR = 10;
     int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080,
             0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    char[][] field, bufferField, tempField;
+    int[][] field;
+    int[][] bufferField;
+    int[][] tempField;
     int[] colors;
     int width = 0;
     int height = 0;
-    float scaleX, scaleY;
+    float scaleX;
+    float scaleY;
     DrawThread drawThread;
 
     public WhirlView(Context context) {
@@ -47,8 +50,8 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
 
     void initField() {
         colors = new int[height * width];
-        field = new char[width][height];
-        bufferField = new char[width][height];
+        field = new int[width][height];
+        bufferField = new int[width][height];
         Random rand = new Random();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -58,37 +61,34 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     void updateColors(int beginX, int beginY, int endX, int endY) {
-        int x2, y2;
         for (int y = beginY; y < endY; y++) {
             for (int x = beginX; x < endX; x++) {
                 bufferField[x][y] = field[x][y];
-                for (int dx = -1; dx <= 1; dx++) {
-                    for (int dy = -1; dy <= 1; dy++) {
-                        x2 = x + dx;
-                        y2 = y + dy;
-                        if (x2 < 0) {
-                            x2 += width;
-                        }
-                        if (y2 < 0) {
-                            y2 += height;
-                        }
-                        if (x2 >= width) {
-                            x2 -= width;
-                        }
-                        if (y2 >= height) {
-                            y2 -= height;
-                        }
-                        if (field[x][y] + 1 == MAX_COLOR) {
-                            if (0 == field[x2][y2]) {
-                                bufferField[x][y] = field[x2][y2];
-                            }
-                        } else {
-                            if (field[x][y] + 1 == field[x2][y2]) {
-                                bufferField[x][y] = field[x2][y2];
-                            }
-                        }
-                    }
+                int newColor = field[x][y] + 1;
+
+                if (newColor >= MAX_COLOR) {
+                    newColor -= MAX_COLOR;
                 }
+
+                if (newColor == field[x - 1][y - 1])
+                    bufferField[x][y] = field[x - 1][y - 1];
+                else if (newColor == field[x - 1][y])
+                    bufferField[x][y] = field[x - 1][y];
+                else if (newColor == field[x - 1][y + 1])
+                    bufferField[x][y] = field[x - 1][y + 1];
+                else if (newColor == field[x][y - 1])
+                    bufferField[x][y] = field[x][y - 1];
+                else if (newColor == field[x][y])
+                    bufferField[x][y] = field[x][y];
+                else if (newColor == field[x][y + 1])
+                    bufferField[x][y] = field[x][y + 1];
+                else if (newColor == field[x + 1][y - 1])
+                    bufferField[x][y] = field[x + 1][y - 1];
+                else if (newColor == field[x + 1][y])
+                    bufferField[x][y] = field[x + 1][y];
+                else if (newColor == field[x + 1][y + 1])
+                    bufferField[x][y] = field[x + 1][y + 1];
+
                 colors[y * width + x] = palette[field[x][y]];
             }
         }
@@ -111,23 +111,83 @@ class WhirlView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     void updateField() {
-        Thread leftTop = new Updater(0, 0, width / 2, height / 2);
+        Thread top = new Updater(1, 1, width - 1, height / 2);
+        Thread bottom = new Updater(1, height / 2, width - 1, height - 1);
+        top.start();
+        bottom.start();
+        int x2, y2;
+        for (int x = 0; x < width; x++) {
+            bufferField[x][0] = field[x][0];
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    x2 = x + dx;
+                    y2 = dy;
+                    if (x2 < 0) x2 += width;
+                    if (y2 < 0) y2 += height;
+                    if (x2 >= width) x2 -= width;
+                    if (y2 >= height) y2 -= height;
+                    if ( (field[x][0] + 1) % MAX_COLOR == field[x2][y2]) {
+                        bufferField[x][0] = field[x2][y2];
+                    }
+                }
+            }
+        }
 
-        Thread rightTop = new Updater(width / 2, 0, width, height / 2);
+        for (int x = 0; x < width; x++) {
+            bufferField[x][height - 1] = field[x][height - 1];
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    x2 = x + dx;
+                    y2 = height - 1 + dy;
+                    if (x2 < 0) x2 += width;
+                    if (y2 < 0) y2 += height;
+                    if (x2 >= width) x2 -= width;
+                    if (y2 >= height) y2 -= height;
+                    if ( (field[x][height - 1] + 1) % MAX_COLOR == field[x2][y2]) {
+                        bufferField[x][height - 1] = field[x2][y2];
+                    }
+                }
+            }
+        }
 
-        Thread leftBottom = new Updater(0, height / 2, width / 2, height);
+        for (int y = 0; y < height; y++) {
+            bufferField[0][y] = field[0][y];
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    x2 = dx;
+                    y2 = y + dy;
+                    if (x2 < 0) x2 += width;
+                    if (y2 < 0) y2 += height;
+                    if (x2 >= width) x2 -= width;
+                    if (y2 >= height) y2 -= height;
+                    if ( (field[0][y] + 1) % MAX_COLOR == field[x2][y2]) {
+                        bufferField[0][y] = field[x2][y2];
+                    }
+                }
+            }
+        }
 
-        Thread rightBottom = new Updater(width / 2, height / 2, width, height);
+        for (int y = 0; y < height; y++) {
+            bufferField[width - 1][y] = field[width - 1][y];
 
-        leftTop.start();
-        rightTop.start();
-        leftBottom.start();
-        rightBottom.start();
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    x2 = width - 1 + dx;
+                    y2 = y + dy;
+                    if (x2 < 0) x2 += width;
+                    if (y2 < 0) y2 += height;
+                    if (x2 >= width) x2 -= width;
+                    if (y2 >= height) y2 -= height;
+                    if ( (field[width - 1][y] + 1) % MAX_COLOR == field[x2][y2]) {
+                        bufferField[width - 1][y] = field[x2][y2];
+                    }
+                }
+            }
+        }
+
         try {
-            leftTop.join();
-            rightTop.join();
-            leftBottom.join();
-            rightBottom.join();
+            top.join();
+            bottom.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
