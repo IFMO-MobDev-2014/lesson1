936b7c954d41f0c69957eaaaf32d1d86e7350bfd
PaulChristmas <pashachristmas@mail.ru>
2014-09-16 00:13:06 +0400

    Рождественский Павел 2536

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -10,16 +10,16 @@ import android.view.SurfaceView;
 import java.util.Random;
 
 /**
-* Created by thevery on 11/09/14.
-*/
+ * Created by thevery on 11/09/14.
+ */
+
 class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
+    int [][] field=null;
+    int width = 240, count=0, sum=0, height=320, scale=4;
     final int MAX_COLOR = 10;
     int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     SurfaceHolder holder;
+    int bitmap[]=null;
     Thread thread = null;
     volatile boolean running = false;
 
@@ -41,24 +41,43 @@ class WhirlView extends SurfaceView implements Runnable {
         } catch (InterruptedException ignore) {}
     }
 
+    public void getFPS(long time){
+        Log.i("", "Fps: " + 1000/time);
+        sum+=1000/time;
+        count++;
+    }
+
     public void run() {
         while (running) {
             if (holder.getSurface().isValid()) {
                 long startTime = System.nanoTime();
                 Canvas canvas = holder.lockCanvas();
+
                 updateField();
+
+                for (int i=0; i<width; i++) {
+                    for (int j=0; j<height; j++) {
+                        bitmap[width*j+i] = palette[field[i][j]];
+                    }
+                }
+
                 onDraw(canvas);
+
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
-                Log.i("TIME", "Circle: " + (finishTime - startTime) / 1000000);
+
+                getFPS((finishTime - startTime) / 1000000);
+
                 try {
                     Thread.sleep(16);
                 } catch (InterruptedException ignore) {}
             }
         }
+        Log.i("","Middle fps: "+ sum/count);
     }
 
     @Override
+
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
         width = w/scale;
         height = h/scale;
@@ -67,6 +86,7 @@ class WhirlView extends SurfaceView implements Runnable {
 
     void initField() {
         field = new int[width][height];
+        bitmap = new int[width*height];
         Random rand = new Random();
         for (int x=0; x<width; x++) {
             for (int y=0; y<height; y++) {
@@ -81,7 +101,6 @@ class WhirlView extends SurfaceView implements Runnable {
             for (int y=0; y<height; y++) {
 
                 field2[x][y] = field[x][y];
-
                 for (int dx=-1; dx<=1; dx++) {
                     for (int dy=-1; dy<=1; dy++) {
                         int x2 = x + dx;
@@ -97,17 +116,17 @@ class WhirlView extends SurfaceView implements Runnable {
                 }
             }
         }
+
         field = field2;
+
     }
 
     @Override
+
     public void onDraw(Canvas canvas) {
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                Paint paint = new Paint();
-                paint.setColor(palette[field[x][y]]);
-                canvas.drawRect(x*scale, y*scale, (x+1)*scale, (y+1)*scale, paint);
-            }
-        }
+
+        canvas.scale(scale, scale);
+        canvas.drawBitmap(bitmap, 0, width, 0, 0, width, height, false, null);
+
     }
 }
