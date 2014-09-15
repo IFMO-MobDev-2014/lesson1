8afba1cd28626a432a1999e75a1d92c4642cbea1
pinguinson <nikita.penguin@gmail.com>
2014-09-15 22:55:09 +0400

    redone using bitmaps

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -1,9 +1,7 @@
 package ru.ifmo.md.lesson1;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -11,27 +9,25 @@ import android.view.SurfaceView;
 import java.util.Random;
 
 /**
- * Created by pinguinson on 13/09/14.
+ * Created by thevery on 11/09/14.
  */
 class WhirlView extends SurfaceView implements Runnable {
     int [][] field = null;
-    boolean [][] changed;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
+    int [][] bufferField = null;
+    int [] cellColor = null;
+    int width = 240;
+    int height = 320;
+    float scaleWidth = 1;
+    float scaleHeight = 1;
     final int MAX_COLOR = 10;
     int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     SurfaceHolder holder;
     Thread thread = null;
     volatile boolean running = false;
-    Paint paint = new Paint();
 
     public WhirlView(Context context) {
         super(context);
         holder = getHolder();
-        if (!isInEditMode()) {
-            setLayerType(SurfaceView.LAYER_TYPE_HARDWARE, null);
-        }
     }
 
     public void resume() {
@@ -47,7 +43,6 @@ class WhirlView extends SurfaceView implements Runnable {
         } catch (InterruptedException ignore) {}
     }
 
-    @SuppressLint("WrongCall")
     public void run() {
         while (running) {
             if (holder.getSurface().isValid()) {
@@ -67,61 +62,56 @@ class WhirlView extends SurfaceView implements Runnable {
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width  = w / scale;
-        height = h / scale;
+        scaleWidth  = (float) w / width;
+        scaleHeight = (float) h / height;
         initField();
     }
 
     void initField() {
-        field   = new int       [width][height];
-        changed = new boolean   [width][height];
+        field       = new int[width][height];
+        bufferField = new int[width][height];
+        cellColor   = new int[width * height];
         Random rand = new Random();
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++) {
                 field[x][y] = rand.nextInt(MAX_COLOR);
-                changed[x][y] = true;
             }
         }
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x = 0; x < width; x++) {
-            System.arraycopy(field[x], 0, field2[x], 0, height);
-            for (int y = 0; y < height; y++) {
-                changed[x][y] = false;
-                for (int dx = 1; dx >= -1; dx--) {
-                    for (int dy = 1; dy >= -1; dy--) {
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++) {
+                bufferField[x][y] = field[x][y];
+                for (int dx=-1; dx<=1; dx++) {
+                    for (int dy=-1; dy<=1; dy++) {
                         int x2 = x + dx;
                         int y2 = y + dy;
-                        if (x2 < 0) x2 += width;
-                        if (y2 < 0) y2 += height;
-                        if (x2 >= width) x2 -= width;
-                        if (y2 >= height)y2 -= height;
-                        if ((field[x][y] + 1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
-                            changed[x][y] = true;
-                            break;
+                        if (x2<0) x2 += width;
+                        if (y2<0) y2 += height;
+                        if (x2>=width) x2 -= width;
+                        if (y2>=height) y2 -= height;
+                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
+                            bufferField[x][y] = field[x2][y2];
                         }
-                    }
-                    if (changed[x][y]) {
-                        break;
                     }
                 }
             }
         }
-        field = field2;
+        field = bufferField;
     }
+
+
+
 
     @Override
     public void onDraw(Canvas canvas) {
-        for (int x = 0; x < width; x++) {
+        for (int x=0; x<width; x++) {
             for (int y = 0; y < height; y++) {
-                if (changed[x][y]) {
-                    paint.setColor(palette[field[x][y]]);
-                    canvas.drawRect(x * scale, y * scale, (x + 1) * scale, (y + 1) * scale, paint);
-                }
+                cellColor[y * width + x] = palette[field[x][y]];
             }
         }
+        canvas.scale(scaleWidth, scaleHeight);
+        canvas.drawBitmap(cellColor, 0, width, 0, 0, width, height, false, null);
     }
 }
