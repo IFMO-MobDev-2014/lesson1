1d1046d352c2e28718a1f480e264aecbf648ab82
Dmitry Nekrasov <d04101995@mail.ru>
2014-09-16 13:59:40 +0400

    border conditions

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -72,7 +72,7 @@ class WhirlView extends SurfaceView implements Runnable {
         field = new int[width][height];
         field2 = new int[width][height];
         pixels = new int[width * height];
-        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
         Random rand = new Random();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -83,57 +83,125 @@ class WhirlView extends SurfaceView implements Runnable {
     }
 
     void updateField() {
-        int previousX;
-        int nextX;
-        int previousY;
-        int nextY;
-
+        
         for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                if (x == 0) {
-                    previousX = width - 1;
-                } else {
-                    previousX = x - 1;
+            if (field[x][0] == MAX_COLOR - 1) {
+                if (field[x][1] == 0) {
+                    field2[x][0] = field[x][1];
                 }
-                if (x == width - 1) {
-                    nextX = 0;
-                } else {
-                    nextX = x + 1;
+                if (field[x][height - 1] == 0) {
+                    field2[x][0] = field[x][height - 1];
                 }
-                if (y == 0) {
-                    previousY = height - 1;
-                } else {
-                    previousY = y - 1;
+            } else {
+                if (field[x][0] + 1 == field[x][1]) {
+                    field2[x][0] = field[x][1];
                 }
-                if (y == height - 1) {
-                    nextY = 0;
-                } else {
-                    nextY = y + 1;
+                if (field[x][0] + 1 == field[x][height - 1]) {
+                    field2[x][0] = field[x][height - 1];
                 }
+            }
+            if (field[x][height - 1] == MAX_COLOR - 1) {
+                if (field[x][height - 2] == 0) {
+                    field2[x][height - 1] = field[x][height - 2];
+                }
+                if (field[x][0] == 0) {
+                    field2[x][height - 2] = field[x][0];
+                }
+            } else {
+                if (field[x][height - 1] + 1 == field[x][height - 2]) {
+                    field2[x][height - 1] = field[x][height - 2];
+                }
+                if (field[x][height - 1] + 1 == field[x][0]) {
+                    field2[x][height - 2] = field[x][0];
+                }
+            }
+        }
 
-                if ((field[x][y] == MAX_COLOR - 1 && field[previousX][previousY] == 0) || (field[x][y] + 1 == field[previousX][previousY])) {
-                    field2[x][y] = field[previousX][previousY];
+        for (int y = 0; y < height; y++) {
+            if (field[0][y] == MAX_COLOR - 1) {
+                if (field[1][y] == 0) {
+                    field2[0][y] = field[1][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[previousX][y] == 0) || (field[x][y] + 1 == field[previousX][y])) {
-                    field2[x][y] = field[previousX][y];
+                if (field[width - 1][y] == 0) {
+                    field2[0][y] = field[width - 1][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[previousX][nextY] == 0) || (field[x][y] + 1 == field[previousX][nextY])) {
-                    field2[x][y] = field[previousX][nextY];
+            } else {
+                if (field[0][y] + 1 == field[1][y]) {
+                    field2[0][y] = field[1][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[x][previousY] == 0) || (field[x][y] + 1 == field[x][previousY])) {
-                    field2[x][y] = field[x][previousY];
+                if (field[0][y] + 1 == field[width - 1][y]) {
+                    field2[0][y] = field[width - 1][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[x][nextY] == 0) || (field[x][y] + 1 == field[x][nextY])) {
-                    field2[x][y] = field[x][nextY];
+            }
+            if (field[width - 1][y] == MAX_COLOR - 1) {
+                if (field[width - 2][y] == 0) {
+                    field2[width - 1][y] = field[width - 2][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[nextX][previousY] == 0) || (field[x][y] + 1 == field[nextX][previousY])) {
-                    field2[x][y] = field[nextX][previousY];
+                if (field[0][y] == 0) {
+                    field2[width - 1][y] = field[0][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[nextX][y] == 0) || (field[x][y] + 1 == field[nextX][y])) {
-                    field2[x][y] = field[nextX][y];
+            } else {
+                if (field[width - 1][y] + 1 == field[width - 2][y]) {
+                    field2[width - 1][y] = field[width - 2][y];
                 }
-                if ((field[x][y] == MAX_COLOR - 1 && field[nextX][nextY] == 0) || (field[x][y] + 1 == field[nextX][nextY])) {
-                    field2[x][y] = field[nextX][nextY];
+                if (field[width - 1][y] + 1 == field[0][y]) {
+                    field2[width - 1][y] = field[0][y];
+                }
+            }
+        }
+        
+        for (int x = 1; x < width - 1; x++) {
+            for (int y = 1; y < height - 1; y++) {
+                if (field[x][y] == MAX_COLOR - 1) {
+                    if (field[x - 1][y - 1] == 0) {
+                        field2[x][y] = field[x - 1][y - 1];
+                    }
+                    if (field[x - 1][y] == 0) {
+                        field2[x][y] = field[x - 1][y];
+                    }
+                    if (field[x - 1][y + 1] == 0) {
+                        field2[x][y] = field[x - 1][y + 1];
+                    }
+                    if (field[x][y - 1] == 0) {
+                        field2[x][y] = field[x][y - 1];
+                    }
+                    if (field[x][y + 1] == 0) {
+                        field2[x][y] = field[x][y + 1];
+                    }
+                    if (field[x + 1][y - 1] == 0) {
+                        field2[x][y] = field[x + 1][y - 1];
+                    }
+                    if (field[x + 1][y] == 0) {
+                        field2[x][y] = field[x + 1][y];
+                    }
+                    if (field[x + 1][y + 1] == 0) {
+                        field2[x][y] = field[x + 1][y + 1];
+                    }
+                } else {
+                    if (field[x][y] + 1 == field[x - 1][y - 1]) {
+                        field2[x][y] = field[x - 1][y - 1];
+                    }
+                    if (field[x][y] + 1 == field[x - 1][y]) {
+                        field2[x][y] = field[x - 1][y];
+                    }
+                    if (field[x][y] + 1 == field[x - 1][y + 1]) {
+                        field2[x][y] = field[x - 1][y + 1];
+                    }
+                    if (field[x][y] + 1 == field[x][y - 1]) {
+                        field2[x][y] = field[x][y - 1];
+                    }
+                    if (field[x][y] + 1 == field[x][y + 1]) {
+                        field2[x][y] = field[x][y + 1];
+                    }
+                    if (field[x][y] + 1 == field[x + 1][y - 1]) {
+                        field2[x][y] = field[x + 1][y - 1];
+                    }
+                    if (field[x][y] + 1 == field[x + 1][y]) {
+                        field2[x][y] = field[x + 1][y];
+                    }
+                    if (field[x][y] + 1 == field[x + 1][y + 1]) {
+                        field2[x][y] = field[x + 1][y + 1];
+                    }
                 }
             }
         }
