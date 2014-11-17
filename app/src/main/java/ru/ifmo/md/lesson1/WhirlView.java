28bdb0895474ac944b0dd777c8381a8bd205a9d5
Artur Malimonov <allpicksucks@gmail.com>
2014-11-17 18:32:17 +0300

    Simplified arrays, added thread work in screen updating. 15 FPS or so.

--- app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
+++ app/src/main/java/ru/ifmo/md/lesson1/WhirlView.java
@@ -2,6 +2,7 @@ package ru.ifmo.md.lesson1;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -11,19 +12,20 @@ import java.util.Random;
 
 /**
  * Created by thevery on 11/09/14.
- * Modified by heat-wave on 15/09/14.
+ * Modified by heat-wave on 17/11/14.
  */
 
 class WhirlView extends SurfaceView implements Runnable {
-    int [][] field = null;
-    int width = 0;
-    int height = 0;
-    int scale = 4;
+    int [] field = null;
+    final int WIDTH = 240;
+    final int HEIGHT = 320;
     final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
+    int [] sc = new int[WIDTH * HEIGHT];
+    int [] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
     Bitmap screen;
     SurfaceHolder holder;
     Thread thread = null;
+    final RectF rect = new RectF(0, 0, 1080, 1920);
     volatile boolean running = false;
 
     public WhirlView(Context context) {
@@ -64,49 +66,53 @@ class WhirlView extends SurfaceView implements Runnable {
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
-        width = w/scale;
-        height = h/scale;
-        screen = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        screen = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.RGB_565);
         initField();
     }
 
     void initField() {
-        field = new int[width][height];
+        field = new int[WIDTH * HEIGHT];
         Random rand = new Random();
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                field[x][y] = rand.nextInt(MAX_COLOR);
-                screen.setPixel(x, y, palette[field[x][y]]);
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                field[x + y * WIDTH] = rand.nextInt(MAX_COLOR);
+                sc[x + y * WIDTH] = palette[field[x + y * WIDTH]];
             }
         }
+        screen.setPixels(sc, 0, WIDTH, 0,0, WIDTH, HEIGHT);
     }
 
     void updateField() {
-        int[][] field2 = new int[width][height];
-        for (int x=0; x<width; x++) {
-            for (int y=0; y<height; y++) {
-                field2[x][y] = field[x][y];
-                for (int dx=-1; dx<=1; dx++) {
-                    for (int dy=-1; dy<=1; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2<0) x2 += width;
-                        if (y2<0) y2 += height;
-                        if (x2>=width) x2 -= width;
-                        if (y2>=height) y2 -= height;
-                        if ( (field[x][y]+1) % MAX_COLOR == field[x2][y2]) {
-                            field2[x][y] = field[x2][y2];
-                            screen.setPixel(x, y, palette[field[x][y]]);
+        new Thread(new Runnable() {
+            public void run() {
+                int[] field2 = new int[WIDTH * HEIGHT];
+                for (int x = 0; x < WIDTH; x++) {
+                    for (int y = 0; y < HEIGHT; y++) {
+                        field2[x + y * WIDTH] = field[x + y * WIDTH];
+                        for (int dx =- 1; dx <= 1; dx++) {
+                            int x2 = x + dx;
+                            if (x2 < 0) x2 += WIDTH;
+                            if (x2 >= WIDTH) x2 -= WIDTH;
+                            for (int dy= -1; dy <= 1; dy++) {
+                                int y2 = y + dy;
+                                if (y2 < 0) y2 += HEIGHT;
+                                if (y2 >= HEIGHT) y2 -= HEIGHT;
+                                if ((field[x + y * WIDTH] + 1) % MAX_COLOR == field[x2 + y2 * WIDTH]) {
+                                    field2[x + y * WIDTH] = field[x2 + y2 * WIDTH];
+                                    sc[x + y * WIDTH] = palette[field2[x + y * WIDTH]];
+                                }
+                            }
                         }
                     }
                 }
+                field = field2;
+                screen.setPixels(sc, 0, WIDTH, 0,0, WIDTH, HEIGHT);
             }
-        }
-        field = field2;
+        }).start();
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(Bitmap.createScaledBitmap(screen, canvas.getWidth(), canvas.getHeight(), true), 0, 0, null);
+        canvas.drawBitmap(screen, null, rect, null);
     }
 }
